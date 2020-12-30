e8cb62569243173ecafb0f9001d13f492fa21338
firewalker06 <didik.wicaksono@gmail.com>
2020-12-30 18:02:24 +0700

    Add links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,9 @@
 class LinksController < ApplicationController
+  def index
+    @links = [
+      ["Twitter", "https://twitter.com/did1k"],
+      ["Twitch", "https://twitch.tv/did1k"],
+      ["Spotify", "https://anchor.fm/di-atas-gudang"]
+    ]
+  end
 end
